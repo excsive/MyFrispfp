dc84c907738bbe050921660523a2ebccd5171789
excsive <114621319+excsive@users.noreply.github.com>
2023-02-21 14:49:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-let snake_case = 10;
-const width = 11;
-console.log(snake_case)
-alert("Hello world")
-let title = 10;
-console.log(title)
-let screens = 10;
-console.log(screens)
-let screenPrice = 10;
+let screenPrice = prompt('Сколько будет стоить данная работа?', "12000")
 console.log(screenPrice)
-let rollback = 10;
-console.log(rollback)
-let fullPrice = 10;
-console.log(fullPrice)
-let adaptive = 10;
-console.log(adaptive)
+
+let servicePrice1 = prompt('Сколько это будет стоить?')
+console.log(servicePrice1)
+
+let servicePrice2 = prompt('Сколько это будет стоить?')
+console.log(servicePrice2)
+
+let fullprice = screenPrice + servicePrice1 + servicePrice2
+
+const allServicePrices = function name(price) {
+    if (price >= 30000) {
+        return "Делаем скидку в 10%"
+    }   else if (price >= 15000 && price < 30000) {
+        return "делаем скидку в 5%"
+    } else if (price >= 0 && price < 15000){
+        return "Скидка не предусмотрена"
+    }
+}
+
+console.log(allServicePrices(fullprice))
